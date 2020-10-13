01417ac00ed069576be19cde1fcb76e111a0191a
Acer <yunthiri1998@gmail.com>
2020-10-13 17:30:04 +0630

    Second updated

--- Exercise1.2.php
+++ Exercise1.2.php
@@ -1,18 +1,23 @@
 <?php
-$hamburger=array('Hamburger', 4.95, 2);
-$milkshake=array('Milk Shake', 1.95, 1);
-$cola=array('Cola', 0.85, 1);
+$hamburger=4.95;
+$milkshake=1.95;
+$cola=0.85;
 $salestax_rate=0.075;
 $pre_taxtip=0.16;
 
-$hamsubtotal = ($hamburger[1] * $hamburger[2]);
-$milkshakesubtotal = $milkshake[1] * $milkshake[2];
-$colasubtotal = $cola[1] * $cola[2];
+$hamsubtotal = ($hamburger *2);
+$milkshakesubtotal = $milkshake* 1;
+$colasubtotal = $cola* 1;
 $subtotal = $hamsubtotal + $milkshakesubtotal + $colasubtotal;
+
 
 $tax = $subtotal * $salestax_rate;
 $tip = $subtotal * $pre_taxtip;
 $total = $subtotal + $tax + $tip;
+// total cost
+echo "<h1>Total cost</h1>";
+echo "Total = $$total";
+echo "<br><hr>";
 ?>
 
 <table width="80%" border="1" cellspacing="0" cellpadding="4">
@@ -23,21 +28,21 @@ $total = $subtotal + $tax + $tip;
 <th>Subtotal</th>
 </tr>
 <tr>
-<td><?php echo "$hamburger[0]";?></td>
-<td><?php echo "$hamburger[1]";?></td>
-<td><?php echo "$hamburger[2]";?></td>
+<td><?php echo "hamburger";?></td>
+<td><?php echo "$4.95";?></td>
+<td><?php echo "2";?></td>
 <td><?php echo "$hamsubtotal";?></td>
 </tr>
 <tr>
-<td><?php echo "$milkshake[0]";?></td>
-<td><?php echo "$milkshake[1]";?></td>
-<td><?php echo "$milkshake[2]";?></td>
+<td><?php echo "milkshake";?></td>
+<td><?php echo "$1.95";?></td>
+<td><?php echo "1";?></td>
 <td><?php echo "$milkshakesubtotal";?></td>
 </tr>
 <tr>
-<td><?php echo "$cola[0]";?></td>
-<td><?php echo "$cola[1]";?></td>
-<td><?php echo "$cola[2]";?></td>
+<td><?php echo "cola";?></td>
+<td><?php echo "$0.85";?></td>
+<td><?php echo "1";?></td>
 <td><?php echo "$colasubtotal";?></td>
 </tr>
 
